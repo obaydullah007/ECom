df131d14db144cd8434d338b75e6d445fa08c1e8
Obaydullah <obaydullahkhaan@gmail.com>
2025-08-06 23:32:42 +0600

    product get api created

--- main.go
+++ main.go
@@ -1,29 +1,94 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request){
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "I'm Obaydullah. I'm Software Engineer.")
 }
 
-func main(){
+type Product struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	ImgUrl      string  `json:"imageUrl"`
+}
+
+var productList []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Please request with GET method", 400)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(productList)
+}
+
+func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", helloHandler)
 
 	mux.HandleFunc("/about", aboutHandler)
 
-	fmt.Println("Server running on port : 3000")
+	mux.HandleFunc("/products", getProducts)
 
-	err := http.ListenAndServe(":3000", mux)
+	fmt.Println("Server running on port : 8080")
+
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("Error starting the server", err)
 	}
+}
+
+func init() {
+	prd1 := Product{
+		ID:          1,
+		Title:       "Telescope",
+		Description: "A Powerful Newtonian Telescope",
+		Price:       199.99,
+		ImgUrl:      "https://i.etsystatic.com/10189331/r/il/204377/3823588808/il_570xN.3823588808_cyr2.jpg",
+	}
+	prd2 := Product{
+		ID:          2,
+		Title:       "Bike",
+		Description: "A Cafe Racer Bike",
+		Price:       2999.99,
+		ImgUrl:      "https://caferacerdreams.es/wp-content/uploads/2016/02/crd11-cafe-racer-crd-honda-cb750kz-portada.jpg",
+	}
+	prd3 := Product{
+		ID:          3,
+		Title:       "Laptop",
+		Description: "A High End Gaming Laptop",
+		Price:       1499.99,
+		ImgUrl:      "https://gagadget.com/media/cache/18/f9/18f934fbff1cdb6f3ff5949b1c3ab9c5.jpg",
+	}
+	prd4 := Product{
+		ID:          4,
+		Title:       "Smartphone",
+		Description: "A Flagship Smartphone",
+		Price:       999.99,
+		ImgUrl:      "https://assets.gadgetandgear.com/upload/product/20240123_1705987892_224395.jpeg",
+	}
+	prd5 := Product{
+		ID:          5,
+		Title:       "Jeep",
+		Description: "A Powerful Off-Road Jeep",
+		Price:       49999.99,
+		ImgUrl:      "https://5.imimg.com/data5/SELLER/Default/2020/9/PW/NC/HD/90810037/willy-open-jeep.jpg",
+	}
+	productList = append(productList, prd1, prd2, prd3, prd4, prd5)
 }
